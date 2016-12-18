703686d5a835c26f94da29c5a6e1c50421f10ef0
Troy1010 <tminus1010@gmail.com>
2016-12-18 17:07:53 -0600

    Regex version complete - worked on first test

--- MergeSafetyChecker/Program.cs
+++ MergeSafetyChecker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MergeSafetyChecker
@@ -19,27 +20,76 @@ namespace MergeSafetyChecker
 
     class MergeSafetyCheckerProgram
     {
-        List<string> ModsRemoved;
         public void Run()
         {
-            int iMatchCount = 0;
-            this.ModsRemoved = File.ReadAllLines(Properties.Settings.Default.SourceFile).ToList();
-            DirectoryInfo DataFolder = new DirectoryInfo(Properties.Settings.Default.DataFolder);
-            foreach (var Mod in DataFolder.EnumerateFiles())
+            //  Establish sRegexPattern
+            string sRegexPattern = "";
+            bool bDoOnce = false;
+            foreach (var sModName in File.ReadAllLines(Properties.Settings.Default.SourceFile).ToList())
             {
-                if (Mod.Extension != ".esp" && Mod.Extension != ".esm") continue;
-                var FileContents = File.ReadAllText(Mod.FullName);
-                foreach (var ModRemoved in ModsRemoved)
+                if (!bDoOnce)
                 {
-                    var MatchableString = "IsModLoaded "+ModRemoved;
-                    if (FileContents.Contains(MatchableString))
+                    bDoOnce = true;
+                }
+                else
+                {
+                    sRegexPattern += "|";
+                }
+                sRegexPattern += sModName;
+            }
+            sRegexPattern = "IsModLoaded \"" + sRegexPattern;
+            //  Search
+            int iTotalMatchCount = 0;
+            foreach (var oMod in (new DirectoryInfo(Properties.Settings.Default.DataFolder)).EnumerateFiles())
+            {
+                // Filter
+                if (oMod.Extension != ".esp" && oMod.Extension != ".esm") continue;
+                //
+                var FileContents = File.ReadAllText(oMod.FullName);
+                var oMatch = Regex.Match(FileContents,sRegexPattern);
+                if (oMatch.Success)
+                {
+                    iTotalMatchCount++;
+                    System.Console.WriteLine($"WARNING: {oMod.Name} contained removed mod {oMatch.Value}");
+                }
+            }
+            System.Console.WriteLine($"Detected {iTotalMatchCount} matches.");
+        }
+    }
+
+    public static class CommonCode
+    {
+        public static object GetRegexMatchedObject(this string MatchedString, Dictionary<object, string[]> d2dMatchables)
+        {
+            foreach (KeyValuePair<object, string[]> vPair in d2dMatchables)
+            {
+                foreach (string sString in vPair.Value)
+                {
+                    if (sString == MatchedString)
                     {
-                        iMatchCount++;
-                        System.Console.WriteLine($"WARNING: {Mod.Name} contained removed mod {ModRemoved}");
+                        return vPair.Key;
                     }
                 }
             }
-            System.Console.WriteLine($"Detected {iMatchCount} errors.");
+            return null;
+        }
+        public static string GetRegexPatternFromMatchables(this Dictionary<object, string[]> d2dMatchables)
+        {
+            bool bDoOnce = false;
+            string sPattern = "";
+            foreach (KeyValuePair<object, string[]> vPair in d2dMatchables)
+            {
+                foreach (string sString in vPair.Value)
+                {
+                    if (!bDoOnce)
+                    {
+                        bDoOnce = true;
+                        sPattern += "|";
+                    }
+                    sPattern += sString;
+                }
+            }
+            return "\b(" + sPattern + ")\b";
         }
     }
 }
