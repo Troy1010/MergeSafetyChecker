9587b84594aeab387a8ca94a7cb9e8e49baf8adc
Justin Swanson <Leviathan1753@gmail.com>
2016-12-18 17:14:57 -0600

    Refactored classes into separate files

---
+++ MergeSafetyChecker/CommonCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSafetyChecker
+{
+    public static class CommonCode
+    {
+        public static object GetRegexMatchedObject(this string MatchedString, Dictionary<object, string[]> d2dMatchables)
+        {
+            foreach (KeyValuePair<object, string[]> vPair in d2dMatchables)
+            {
+                foreach (string sString in vPair.Value)
+                {
+                    if (sString == MatchedString)
+                    {
+                        return vPair.Key;
+                    }
+                }
+            }
+            return null;
+        }
+        public static string GetRegexPatternFromMatchables(this Dictionary<object, string[]> d2dMatchables)
+        {
+            bool bDoOnce = false;
+            string sPattern = "";
+            foreach (KeyValuePair<object, string[]> vPair in d2dMatchables)
+            {
+                foreach (string sString in vPair.Value)
+                {
+                    if (!bDoOnce)
+                    {
+                        bDoOnce = true;
+                        sPattern += "|";
+                    }
+                    sPattern += sString;
+                }
+            }
+            return "\b(" + sPattern + ")\b";
+        }
+    }
+}
---
+++ MergeSafetyChecker/MergeSafetyCheckerProgram.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MergeSafetyChecker
+{
+    public class MergeSafetyCheckerProgram
+    {
+        public void Run()
+        {
+            //  Establish sRegexPattern
+            string sRegexPattern = "";
+            bool bDoOnce = false;
+            foreach (var sModName in File.ReadAllLines(Properties.Settings.Default.SourceFile).ToList())
+            {
+                if (!bDoOnce)
+                {
+                    bDoOnce = true;
+                }
+                else
+                {
+                    sRegexPattern += "|";
+                }
+                sRegexPattern += sModName;
+            }
+            sRegexPattern = "IsModLoaded \"" + sRegexPattern;
+            //  Search
+            int iTotalMatchCount = 0;
+            foreach (var oMod in (new DirectoryInfo(Properties.Settings.Default.DataFolder)).EnumerateFiles())
+            {
+                // Filter
+                if (oMod.Extension != ".esp" && oMod.Extension != ".esm") continue;
+                //
+                var FileContents = File.ReadAllText(oMod.FullName);
+                var oMatch = Regex.Match(FileContents, sRegexPattern);
+                if (oMatch.Success)
+                {
+                    iTotalMatchCount++;
+                    System.Console.WriteLine($"WARNING: {oMod.Name} contained removed mod {oMatch.Value}");
+                }
+            }
+            System.Console.WriteLine($"Detected {iTotalMatchCount} matches.");
+        }
+    }
+}
